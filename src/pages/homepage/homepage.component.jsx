19afe225eeff574a42c0733eb40ce556da738017
samira-ouraga <samiraob71@gmail.com>
2020-06-09 16:03:14 -0700

    added homepage with text

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'> THIS IS THE HOMEPAGE FOR BLACKSPACE
+    <div>signup</div>
+    <div>signin</div>
+    </div>
+);
+
+export default HomePage;
