0817257b79b258f20a5b04c345aa5baeb852895d
samira-ouraga <samiraob71@gmail.com>
2020-06-09 16:34:14 -0700

    added sign-in and signup page and routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route, Switch, Redirect} from 'react-router-dom';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 
 class App extends React.Component {
   render() {
     return (
-      <div className='App'>
-        <HomePage />
+      <div>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/signin'  component={SignInAndSignUpPage} />
+          </Switch>
+      
       </div>
     );
   }
