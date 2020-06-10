daf5b0b1bda10984d29fb12fc8835197d69f8e7c
samira-ouraga <samiraob71@gmail.com>
2020-06-09 17:35:55 -0700

    added the sign up UI, no functionality yet

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -4,12 +4,13 @@ import SignIn from '../../components/sign-in/sign-in.component';
 
 
 import './sign-in-and-sign-up.styles.scss';
+import SignUp from '../../components/sign-up/sign-up.components';
 
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
-       <div>sign up</div>
+        <SignUp />
     </div>
 );
 
