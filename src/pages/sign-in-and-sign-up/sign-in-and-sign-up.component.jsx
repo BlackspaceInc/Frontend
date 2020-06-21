399305dcde08dddfb73162e73e19ddf2f2341496
samira-ouraga <samiraob71@gmail.com>
2020-06-20 17:01:34 -0700

    added tests for components rendering and styles for signin and signup pages

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -4,7 +4,7 @@ import SignIn from '../../components/sign-in/sign-in.component';
 
 
 import './sign-in-and-sign-up.styles.scss';
-import SignUp from '../../components/sign-up/sign-up.components';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 
 const SignInAndSignUpPage = () => (
