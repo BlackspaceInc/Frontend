da84a414e156d67a7e4309d626e9f7643748a0c0
samira-ouraga <samiraob71@gmail.com>
2020-06-23 19:08:18 -0700

    improved product list component with test

--- src/components/product-list/product-list.component.jsx
+++ src/components/product-list/product-list.component.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import './product-list.styles.scss';
-import {Product} from '../../components/product/product.component';
-
+import Product from '../../components/product/product.component';
 
 
 export const ProductList = () => (
-    //holder 
-    
-    <div className='product-list'>
-        <Product/>
+    //add logic to filter and show some cards 
+    <div className='collection-preview'>
+        <h1 className='title'>Category</h1> 
+        <div className='preview'>
+            <Product/>
+        </div>
     </div>
+
+
 );
 
