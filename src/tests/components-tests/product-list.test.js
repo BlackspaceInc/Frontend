0f03e8b833708e03484fb287833c03cf5ee04c3e
samira-ouraga <samiraob71@gmail.com>
2020-06-23 20:06:57 -0700

    modified product list component

--- src/tests/components-tests/product-list.test.js
+++ src/tests/components-tests/product-list.test.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Product from '../../components/product.component.jsx';
+import {ProductList} from '../../components/product-list/product-list.component';
+import {Product} from '../../components/product/product.component';
 
 
-describe('ProductlistComponent', () =>{
+
+describe('Product-list component', () => {
     let wrapper;
   
-    beforeEach(() => wrapper = shallow(<SignInAndSignUpPage />));
+    beforeEach(() => wrapper = shallow(<ProductList />));
   
     it('should render a <div />', () => {
-      expect(wrapper.find('div').length).toEqual(1);
+      expect(wrapper.find('div').length).toEqual(2);
     });
-
+  
     it('should render a <h1 />', () => {
         expect(wrapper.find('h1').length).toEqual(1);
-    });
+      });
 
     it('should render the Product Component', () => {
         expect(wrapper.containsMatchingElement(< Product/>)).toEqual(true);
-    });
-})
+      });
+  });
