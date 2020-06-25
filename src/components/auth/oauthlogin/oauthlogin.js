082d512ddab609217449c7eb69ad3a524257b781
LensPlatform <iinvestplatform@gmail.com>
2020-06-25 15:40:24 -0700

    added oauth registration component and ogin

---
+++ src/components/auth/oauthlogin/oauthlogin.js
@@ -0,0 +1,96 @@
+import React, {Fragment, useState} from "react";
+import "./oauthlogin.scss"
+import {Redirect} from "react-router-dom";
+import {containsInteger, isEmpty} from "../../../utilities/validation/validation";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {setAlert} from "../../../actions/alert";
+import {register} from "../../../actions/auth";
+
+const OauthLogin = ({setAlert, register, isAuthenticated }) => {
+    const [formData, setFormData] = useState({
+        firstname: '',
+        lastname: '',
+        email: '',
+        username: '',
+        password: '',
+        password2: ''
+    });
+
+    const { firstname, lastname, email, username, password, password2 } = formData;
+
+    const onChange = (e) =>
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        if (password !== password2) {
+            setAlert('Passwords do not match', 'danger');
+        }
+
+        if(password.length <= 10){
+            setAlert("Password must contain more than 10 characters")
+        }
+
+        if(!containsInteger(password)){
+            setAlert("Password must contain an integers")
+        }
+
+        if(isEmpty(username)){
+            setAlert("Username cannot be null")
+        }
+
+        if(isEmpty(email)){
+            setAlert("Email cannot be null")
+        }
+
+        if(isEmpty(firstname) || isEmpty(lastname)){
+            setAlert("Firstname or Lastname cannot be null")
+        }
+
+        register({ email, firstname, lastname, password, username });
+    };
+
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />;
+    }
+
+    return (
+        <Fragment>
+        <div id="login-box">
+            <div className="left">
+                <h1>Sign up with email</h1>
+                <form onSubmit={onSubmit}>
+                    <input type="firstname" name="firstname" placeholder="FirstName" value={firstname} onChange={onChange} required/>
+                    <input type="lastname" name="lastname" placeholder="LastName" value={lastname} onChange={onChange} required/>
+                    <input type="username" name="username" placeholder="UserName" value={username} onChange={onChange} required/>
+                    <input type="email" name="email" placeholder="E-mail" value={email} onChange={onChange} required/>
+                    <input type="password" name="password" placeholder="Password" value={password} onChange={onChange} required/>
+                    <input type="password" name="password2" placeholder="Confirm Password" value={password2} onChange={onChange} required/>
+                    <input type="submit" className="btn btn-primary" value="Submit" />
+                </form>
+            </div>
+
+            <div className="right">
+                <span className="loginwith">Sign up with<br/>social network</span>
+                <button className="social-signup facebook">Sign up with Facebook</button>
+                <button className="social-signup linkedin">Sign up with LinkedIn</button>
+                <button className="social-signup twitter">Sign up with Twitter</button>
+                <button className="social-signup google">Sign up with Google+</button>
+            </div>
+        </div>
+        </Fragment>
+    )
+}
+
+OauthLogin.propTypes = {
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { setAlert, register })(OauthLogin);
--- src/components/routing/routes.js
+++ src/components/routing/routes.js
@@ -6,15 +6,16 @@ import NotFound from "../layout/layout";
 import Alert from "../layout/alert";
 import Login from "../auth/login";
 import Register from "../auth/register";
+import OauthLogin from "../auth/oauthlogin/oauthlogin";
 
 const Routes = props => {
     return (
         <section className="container">
             <Alert />
             <Switch>
-                <Route exact path="/register" component={Register} />
                 <Route exact path="/login" component={Login} />
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <Route exact path="/register" component={OauthLogin}/>
                 <Route component={NotFound} />
             </Switch>
         </section>
