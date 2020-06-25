cb3501b299fb182fcd8382d4ea166a3e26a39058
LensPlatform <iinvestplatform@gmail.com>
2020-06-24 19:26:06 -0700

    added redux router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './App.css';
-import {Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utilities/authentication/authentication';
+
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 
-class App extends React.Component {
-  render() {
+const App = () => {
+    useEffect(() => {
+            setAuthToken(localStorage.token);
+            store.dispatch(loadUser());
+        }, []);
+
     return (
-      <div>
-          <Header />
-          <Switch>
-            <Route exact path='/'  component={HomePage} />
-            <Route path='/signin'  component={SignInAndSignUpPage} />
-          </Switch>
-      
-      </div>
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Header />
+                    <Switch>
+                        <Route exact path='/'  component={HomePage} />
+                        <Route path='/signin'  component={SignInAndSignUpPage} />
+                    </Switch>
+                </Fragment>
+            </Router>
+        </Provider>
     );
-  }
-}
+};
 
 export default App;
---
+++ src/utilities/validation/validation.js
@@ -0,0 +1,2 @@
+// export a global handler check for empty ness across numerous types
+export var isEmpty =  value => value == null || false || (typeof value === "object" && Object.keys(value).length === 0) ||  (typeof value === "string" && value.trim().length === 0);
