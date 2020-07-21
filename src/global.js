f1d9ced5c44abde45b02aa78482c35e3d1a3ce44
LensPlatform <iinvestplatform@gmail.com>
2020-07-21 02:52:55 -0400

    added hardcoded service address to expedite deployment

--- src/global.js
+++ src/global.js
@@ -3,6 +3,6 @@ import { serviceAddressSubscript } from "./utilities/addressResolution/addressRe
 var suffix = serviceAddressSubscript()
 
 export const backend = {
-    user_service: suffix !== '' ? 'http://user-service.' + suffix + ":9100/" : 'http://user-service:9100/',
-    company_management_service: suffix !== '' ? 'http://company-management-service.' + suffix + ':9898/query' : 'http://company-management-service:9898/query',
+    user_service: suffix !== '' ? 'http://user-service.' + suffix + ":9100/" : 'http://user-service.default.svc.cluster.local:9100/',
+    company_management_service: suffix !== '' ? 'http://company-management-service.' + suffix + ':9898/query' : 'http://company-management-service.default.svc.cluster.local:9898/query',
 }
