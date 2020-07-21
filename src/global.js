ce8d1d52c1fd2e5aa2d19d07287f80c610813ac0
LensPlatform <iinvestplatform@gmail.com>
2020-07-21 02:00:40 -0400

    staging

--- src/global.js
+++ src/global.js
@@ -4,5 +4,5 @@ var suffix = serviceAddressSubscript()
 
 export const backend = {
     user_service: suffix !== '' ? 'http://user-service.' + suffix + ":9100/" : 'http://user-service:9100/',
-    company_management_service: suffix !== '' ? 'http://company-management-service.' + suffix + ':9898/query' : 'http://localhost:9898/query',
+    company_management_service: suffix !== '' ? 'http://company-management-service.' + suffix + ':9898/query' : 'http://company-management-service:9898/query',
 }
