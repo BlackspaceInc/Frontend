97b4e169599e2f16bd683a6c248f0b09e9377b3d
LensPlatform <iinvestplatform@gmail.com>
2020-07-22 19:44:15 -0400

    fixed deployment issue with nginx fix

--- src/global.js
+++ src/global.js
@@ -3,6 +3,6 @@ import { serviceAddressSubscript } from "./utilities/addressResolution/addressRe
 var suffix = serviceAddressSubscript()
 
 export const backend = {
-    user_service: 'http://68.183.248.735:9100/',
-    company_management_service: 'http://138.197.232.159:9898/query',
+    user_service: '/v1',
+    company_management_service: '/query',
 }
