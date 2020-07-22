e5de393d3293b2f51e5fb9c9dcb9589afb12c9ff
LensPlatform <iinvestplatform@gmail.com>
2020-07-22 04:45:46 -0400

    newest fix for deployment expedition

--- src/global.js
+++ src/global.js
@@ -2,10 +2,7 @@ import { serviceAddressSubscript } from "./utilities/addressResolution/addressRe
 
 var suffix = serviceAddressSubscript()
 
-const userServiceIp = '157.230.197.135'
-const companyServiceIp = '157.230.198.168'
-
 export const backend = {
-    user_service: suffix !== '' ? 'http://user-service.' + suffix + ":9100/" : 'http://' + userServiceIp + ':9100/',
-    company_management_service: suffix !== '' ? 'http://company-management-service.' + suffix + ':9898/query' : 'http://' + companyServiceIp + ':9898/query',
+    user_service: 'http://157.230.197.135:9100/',
+    company_management_service: 'http://157.230.198.168:9898/query',
 }
