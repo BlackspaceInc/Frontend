b9c3a3cf7bf123d2ef78f4baaf35bc22caa3a7de
samira-ouraga <samiraob71@gmail.com>
2020-08-05 10:33:46 -0700

    changes

--- src/pages/footerpage/footerpage.component.jsx
+++ src/pages/footerpage/footerpage.component.jsx
@@ -11,7 +11,6 @@ const FooterPage = () => {
           <h1 className='blk-logo-text'>
             BlkSpace Company
           </h1>
-          <p> Lorem ipsum dolor, sit amet</p>
           <div className='contact'>
             {/* <span><i className='fa fa-phone'></i> &nbsp; 000-000-0000</span>
             <span><i className='fas fa-envelope'></i>&nbsp; fake@doe.com</span> */}
@@ -19,8 +18,6 @@ const FooterPage = () => {
           <div className='socials'>
             <SocialIcon network="twitter" url='#' style={{ height: 25, width: 25, margin:5}} />
             <SocialIcon network="instagram" url='#' style={{ height: 25, width: 25, margin:5}} />
-            <SocialIcon network="facebook" url='#' style={{ height: 25, width: 25, margin:5 }} />
-            <SocialIcon network="linkedin" url='#' style={{ height: 25, width: 25, margin:5 }} />
           </div>
         </div>
       </div>
@@ -28,42 +25,7 @@ const FooterPage = () => {
         &copy; BlkSpace 
       </div>
     </div>
-    // <MDBFooter color="blue" className="font-small pt-4 mt-4 ">
-    //   <MDBContainer fluid className="text-center text-md-left">
-    //     <MDBRow>
-    //       <MDBCol md="6">
-    //         <h5 className="title">Footer Content</h5>
-    //         <p>
-    //           Here you can use rows and columns here to organize your footer
-    //           content.
-    //         </p>
-    //       </MDBCol>
-    //       <MDBCol md="6">
-    //         <h5 className="title">Links</h5>
-    //         <ul>
-    //           <li className="list-unstyled">
-    //             <a href="#!">Link 1</a>
-    //           </li>
-    //           <li className="list-unstyled">
-    //             <a href="#!">Link 2</a>
-    //           </li>
-    //           <li className="list-unstyled">
-    //             <a href="#!">Link 3</a>
-    //           </li>
-    //           <li className="list-unstyled">
-    //             <a href="#!">Link 4</a>
-    //           </li>
-    //         </ul>
-    //       </MDBCol>
-    //     </MDBRow>
-    //   </MDBContainer>
-    //   <div className="footer-copyright text-center py-3">
-    //     <MDBContainer fluid>
-    //       &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
-    //       <p>social media links</p>
-    //     </MDBContainer>
-    //   </div>
-    // </MDBFooter>
+
   );
 }
 
