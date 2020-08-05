d566b185c118ede1d557ac0fe59380e75e0e75f6
samira-ouraga <samiraob71@gmail.com>
2020-08-05 10:48:18 -0700

    changed colors

--- src/pages/about-us/about-us.component.jsx
+++ src/pages/about-us/about-us.component.jsx
@@ -7,12 +7,10 @@ const AboutUs = () => {
     return(
         <div className='blk-about'>
             <div className="about-section">
-                <h1>About Us Page</h1>
-                <p>why are we doing this?</p>
-                <p>our mission statement, values and core principles</p>
+                <h1>FOUNDERS</h1>
+                {/* <p>why are we doing this?</p>
+                <p>our mission statement, values and core principles</p> */}
             </div>
-
-            <h2 className='blk-about-h2'>Our Team</h2>
 
             <div className="row">
                 <div className="column">
@@ -20,7 +18,7 @@ const AboutUs = () => {
                     <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" alt="Jane" className='blk-img'/>
                     <div className="container">
                         <h2>ALEXIS SERRA</h2>
-                        <p className="title">position</p>
+                        <p className="title">BUSINESS</p>
                         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                         
                     </div>
@@ -31,7 +29,7 @@ const AboutUs = () => {
                     <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" alt="Mike" className='blk-img'/>
                     <div className="container">
                         <h2>JERED BUTLER</h2>
-                        <p className="title">position</p>
+                        <p className="title">BUSINESS</p>
                         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                     </div>
                     </div>
@@ -41,7 +39,7 @@ const AboutUs = () => {
                     <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" alt="John" className='blk-img'/>
                     <div className="container">
                         <h2>CALEB ATEMIE</h2>
-                        <p className="title">postiion</p>
+                        <p className="title">BUSINESS</p>
                         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                     </div>
                     </div>
@@ -51,7 +49,7 @@ const AboutUs = () => {
                     <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" alt="John" className='blk-img'/>
                     <div className="container">
                         <h2>CAMERON BURFORD</h2>
-                        <p className="title">position</p>
+                        <p className="title">ENGINEERING</p>
                         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                     </div>
                     </div>
@@ -61,7 +59,7 @@ const AboutUs = () => {
                     <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" alt="John" className='blk-img'/>
                     <div className="container">
                         <h2>SAMIRA OURAGA</h2>
-                        <p className="title">position</p>
+                        <p className="title">ENGINEERING</p>
                         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                     </div>
                     </div>
@@ -71,7 +69,7 @@ const AboutUs = () => {
                     <img src="https://react.semantic-ui.com/images/avatar/large/matthew.png" alt="John" className='blk-img'/>
                     <div className="container">
                         <h2>YOAN YOMBA</h2>
-                        <p className="title">position</p>
+                        <p className="title">ENGINEERING</p>
                         <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                     </div>
                     </div>
--- src/pages/footerpage/footerpage.component.jsx
+++ src/pages/footerpage/footerpage.component.jsx
@@ -12,22 +12,15 @@ const FooterPage = () => {
           <h1 className='blk-logo-text'>
             BLKSPACE COMPANY
           </h1>
-<<<<<<< HEAD
-=======
           <h5><span>Connecting you to black-owned business</span></h5>
->>>>>>> 9f4ceb1164b9a57a4dc467929e55e2122f484ed9
           <div className='contact'>
             {/* <span><i className='fa fa-phone'></i> &nbsp; 000-000-0000</span> */}
             {/* <span><i className='fas fa-envelope'></i> Contacs us: blkspace2020@gmail.com </span> */}
           </div>
           <div className='socials'>
-<<<<<<< HEAD
-            <SocialIcon network="twitter" url='#' style={{ height: 25, width: 25, margin:5}} />
-            <SocialIcon network="instagram" url='#' style={{ height: 25, width: 25, margin:5}} />
-=======
             <SocialIcon network="twitter" url='https://twitter.com/therealblkspace/status/1282532400665227269?s=21' target='blank' style={{ height: 25, width: 25, margin:5}} />
             <SocialIcon network="instagram" url='https://www.instagram.com/theblkspaces/' target='blank' style={{ height: 25, width: 25, margin:5}} />
->>>>>>> 9f4ceb1164b9a57a4dc467929e55e2122f484ed9
+
           </div>
         </div>
       </div>
@@ -35,11 +28,7 @@ const FooterPage = () => {
         &copy; BLKSPACE
       </div>
     </div>
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 9f4ceb1164b9a57a4dc467929e55e2122f484ed9
   );
 }
 
